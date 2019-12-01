96c90df0fbd8778f432549a2c06b5ae1b01d5710
VGimonnet <valentin.gimonnet18@gmail.com>
2019-12-01 21:10:40 +0100

    Initialisation projet

---
+++ src/Controller/MusiqueController.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Musique;
+
+
+/**
+ * @Route("/api_musique", name="api_musique")
+ */
+class MusiqueController extends AbstractController
+{
+    /**
+     * @Route("/indexmusique", name="index_musique")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MusiqueController.php',
+        ]);
+    }
+
+    /**
+     * @Route("/musiques", name="musiques", methods={"GET"})
+     */
+    public function getAllMusiques(){
+        $repository = $this->getDoctrine()->getRepository(Musique::class);
+        $musiques = $repository->findAll();
+        $listMusiques = [];
+
+        foreach($musiques as $musique){
+            $listMusiques[] = array(
+                'id' => $musique->getId(),
+                'titre' => $musique->getTitre(),
+                'artiste' => $musique->getArtiste(),
+                'album' => $musique->getAlbum(),
+                'annee' => $musique->getAnnee(),
+                'genre' => $musique->getGenre(),
+                'pathimage' => $musique->getPathimage(),
+                'pathmusique' => $musique->getPathmusique()
+            ); 
+        }
+
+        $reponse = new Response();
+        $reponse->setContent(json_encode(array("musiques"=>$listMusiques)));
+        $reponse->headers->set("Content-Type", "application/json");
+        $reponse->headers->set("Access-Control-Allow-Origin", "*");
+        return $reponse;
+    }
+
+    /**
+     * @Route("/musiques/{id}", name="musiques_id", methods={"GET"})
+     */
+    public function getMusiqueId($id){
+        $repository = $this->getDoctrine()->getRepository(Musique::class);
+        $musique = $repository->find($id);
+
+        if( !empty($musique)){
+            $detailMusique = array(
+                'id' => $musique->getId(),
+                'titre' => $musique->getTitre(),
+                'artiste' => $musique->getArtiste(),
+                'album' => $musique->getAlbum(),
+                'annee' => $musique->getAnnee(),
+                'genre' => $musique->getGenre(),
+                'pathimage' => $musique->getPathimage(),
+                'pathmusique' => $musique->getPathmusique()
+            );
+        }        
+
+        $reponse = new Response();
+        $reponse->setContent(json_encode(array("musique"=>$detailMusique)));
+        $reponse->headers->set("Content-Type", "application/json");
+        $reponse->headers->set("Access-Control-Allow-Origin", "*");
+        return $reponse;
+    }
+
+    /**
+     * @Route("/musiques/ajouter/{titre}/{artiste}/{album}/{annee}/{genre}/{pathimage}/{pathmusique}", name="musique_ajout", methods={"POST"})
+     */
+    public function ajouterMusique($titre, $artiste, $album = null, $annee = null, $genre = null, $pathimage = null, $pathmusique){
+        if($titre != null and $artiste != null and $pathmusique != null){
+            $em = $this->getDoctrine()->getManager();
+            $musique = new Musique();
+            $musique->setTitre($titre);
+            $musique->setArtiste($artiste);
+            $musique->setAlbum($album);
+            $musique->setAnnee($annee);
+            $musique->setGenre($genre);
+            $musique->setPathimage($pathimage);
+            $musique->setPathmusique($pathmusique);
+    
+            $em->persist($musique);
+            $em->flush();
+    
+            $reponse = new Response(json_encode(array(
+                'id'     => $musique->getId(),
+                'artiste'    => $musique->getArtiste(),
+                'titre' => $musique->getTitre()
+                )
+            ));
+        }else{
+            $reponse = new Response(json_encode(array('erreur' => 'musique non valide')));
+        }
+        
+
+        $reponse->headers->set("Content-Type", "application/json");
+        $reponse->headers->set("Access-Control-Allow-Origin", "*");
+        return $reponse;
+    }
+
+    /**
+     * @Route("/musiques/delete/{id}", name="musique_delete", methods={"DELETE"})
+     */
+    public function deleteMusique($id){
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Musique::class);
+        $musique = $repository->find($id);
+        $em->remove($musique);
+        $em->flush();
+
+        $reponse = new Response(json_encode(array(
+            'artiste'    => $musique->getArtiste(),
+            'titre' => $musique->getTitre(),
+            ))
+        );
+        $reponse->headers->set("Content-Type", "application/json");
+        $reponse->headers->set("Access-Control-Allow-Origin", "*");
+        return $reponse;
+    }
+
+        /**
+     * @Route("/musiques/ajouter/{id}/{titre}/{artiste}/{album}/{annee}/{genre}/{pathimage}/{pathmusique}", name="musique_ajout", methods={"POST"})
+     */
+    public function modifierMusique($id, $titre, $artiste, $album, $annee, $genre, $pathimage, $pathmusique){
+        $em = $this->getDoctrine()->getManager();
+        $repository = $this->getDoctrine()->getRepository(Musique::class);
+        $musique = $repository->find($id);
+        
+        $musique->setTitre($titre);
+        $musique->setArtiste($artiste);
+        $musique->setAlbum($album);
+        $musique->setAnnee($annee);
+        $musique->setGenre($genre);
+        $musique->setPathimage($pathimage);
+        $musique->setPathmusique($pathmusique);
+
+        $em->persist($musique);
+        $em->flush();
+
+        $reponse = new Response(json_encode(array(
+            'id'     => $musique->getId(),
+            'artiste'    => $musique->getArtiste(),
+            'titre' => $musique->getTitre()
+            )
+        ));
+
+        $reponse->headers->set("Content-Type", "application/json");
+        $reponse->headers->set("Access-Control-Allow-Origin", "*");
+        return $reponse;
+    }
+}
+
+
---
+++ src/Entity/Musique.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MusiqueRepository")
+ */
+class Musique
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $artiste;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $album;
+
+    /**
+     * @ORM\Column(type="smallint", nullable=true)
+     */
+    private $annee;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $genre;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $pathimage;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pathmusique;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getArtiste(): ?string
+    {
+        return $this->artiste;
+    }
+
+    public function setArtiste(string $artiste): self
+    {
+        $this->artiste = $artiste;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?string
+    {
+        return $this->album;
+    }
+
+    public function setAlbum(?string $album): self
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    public function getAnnee(): ?int
+    {
+        return $this->annee;
+    }
+
+    public function setAnnee(?int $annee): self
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getPathimage(): ?string
+    {
+        return $this->pathimage;
+    }
+
+    public function setPathimage(?string $pathimage): self
+    {
+        $this->pathimage = $pathimage;
+
+        return $this;
+    }
+
+    public function getPathmusique(): ?string
+    {
+        return $this->pathmusique;
+    }
+
+    public function setPathmusique(string $pathmusique): self
+    {
+        $this->pathmusique = $pathmusique;
+
+        return $this;
+    }
+}
