554769728b573259528f2cfca6cbf0786b539a36
VGimonnet <valentin.gimonnet18@gmail.com>
2019-12-02 21:28:31 +0100

    Ajout .sql pour importation

---
+++ public/api_musique.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  lun. 02 déc. 2019 à 20:26
+-- Version du serveur :  10.4.10-MariaDB
+-- Version de PHP :  5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `api_musique`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `migration_versions`
+--
+
+DROP TABLE IF EXISTS `migration_versions`;
+CREATE TABLE IF NOT EXISTS `migration_versions` (
+  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `migration_versions`
+--
+
+INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
+('20191201162513', '2019-12-01 16:43:53');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `musique`
+--
+
+DROP TABLE IF EXISTS `musique`;
+CREATE TABLE IF NOT EXISTS `musique` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `artiste` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `album` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `annee` smallint(6) DEFAULT NULL,
+  `genre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pathimage` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pathmusique` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `musique`
+--
+
+INSERT INTO `musique` (`id`, `titre`, `artiste`, `album`, `annee`, `genre`, `pathimage`, `pathmusique`) VALUES
+(1, 'Back in Black', 'AC-DC', 'Back in Black', 1980, 'Hard Rock', 'App/public/Images/Acdc_-_Back_In_Black-front', 'App/public/Musiques/AC-DC - Back in Black.mp3'),
+(2, 'Chop Suey!', 'System Of A Down', 'Chop Suey!', 2001, 'Metal', 'public/Images/chop_suey_album_front.jpg', 'public/Musiques/soad_chop_suey.mp3'),
+(3, 'Lose Yourself', 'Eminem', '8Mile', 2002, 'Rap', 'public/Images/lose_yourself.jpg', 'public/Musiques/Eminem-Berzerk.mp3');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
