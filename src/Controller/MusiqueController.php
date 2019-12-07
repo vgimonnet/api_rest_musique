b802b140d209d1a2e554ff583b9a05489dd99f62
VGimonnet <valentin.gimonnet18@gmail.com>
2019-12-07 16:29:00 +0100

    Modif route POST, getMusiqueFile et getImageFile

--- src/Controller/MusiqueController.php
+++ src/Controller/MusiqueController.php
@@ -85,16 +85,42 @@ class MusiqueController extends AbstractController
      * @Route("/musiques/ajouter/{titre}/{artiste}/{album}/{annee}/{genre}/{pathimage}/{pathmusique}", name="musique_ajout", methods={"POST"})
      */
     public function ajouterMusique($titre, $artiste, $album = null, $annee = null, $genre = null, $pathimage = null, $pathmusique){
-        if($titre != null and $artiste != null and $pathmusique != null){
+
+        if($_POST['titre'] != null and $_POST['titre'] != null and $_FILES['musique']['name'] != null){
             $em = $this->getDoctrine()->getManager();
             $musique = new Musique();
-            $musique->setTitre($titre);
-            $musique->setArtiste($artiste);
-            $musique->setAlbum($album);
-            $musique->setAnnee($annee);
-            $musique->setGenre($genre);
-            $musique->setPathimage($pathimage);
-            $musique->setPathmusique($pathmusique);
+            $musique->setTitre($_POST['titre']);
+            $musique->setArtiste($_POST['artiste']);
+
+            if($_POST['album'] != null){
+                $musique->setAlbum($_POST['album']);
+            }else{
+                $musique->setAlbum(null);
+            }
+
+            if($_POST['album'] != null){
+                $musique->setAnnee($_POST['annee']);
+            }else{
+                $musique->setAnnee(null);
+            }
+            
+            if($_POST['album'] != null){
+                $musique->setGenre($_POST['genre']);
+            }else{
+                $musique->setGenre(null);
+            }
+            
+            if($_FILES['image']['name'] != null){
+                $musique->setPathimage($_FILES['image']['name']);
+
+            }else{
+                $musique->setPathimage(null);
+            }
+
+            
+            $musique->setPathmusique($_FILES['musique']['name']);
+
+
     
             $em->persist($musique);
             $em->flush();
@@ -174,22 +200,29 @@ class MusiqueController extends AbstractController
         $repository = $this->getDoctrine()->getRepository(Musique::class);
         $musique = $repository->find($id);
 
+
+
         $file = "../public/Musiques/".$musique->getPathmusique();
         
+
         return new \Symfony\Component\HttpFoundation\BinaryFileResponse($file);
     }
 
     /**
-     * @Route("/musiques/get/image/{id}", name="get_musique", methods={"GET"})
+     * @Route("/musiques/get/image/{id}", name="get_image", methods={"GET"})
      */
     public function getImageFile($id){
 
         $repository = $this->getDoctrine()->getRepository(Musique::class);
         $musique = $repository->find($id);
 
-        $file = "../public/Images/".$musique->getPathmusique();
+        $file = "../public/Images/".$musique->getPathimage();
         
         return new \Symfony\Component\HttpFoundation\BinaryFileResponse($file);
     }
+
+    /**
+     * 
+     */
 
 }
