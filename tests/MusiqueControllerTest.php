51d1cf1c1fcdcf1cc75d108e7f879e452303c477
VGimonnet <valentin.gimonnet18@gmail.com>
2019-12-21 14:04:14 +0100

    Ajout de  tests pour MusiqueController

--- tests/MusiqueControllerTest.php
+++ tests/MusiqueControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Controller\MusiqueController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MusiqueControllerTest extends WebTestCase{
@@ -20,7 +21,31 @@ class MusiqueControllerTest extends WebTestCase{
 
     public function test_Post_Musiques(){
         $client = static::createClient();
-        $client->request('POST', 'https://localhost:8000/api_musique/musiques/ajouter/test/test/null/null/null/null/test');
+        $client->request('POST', 'https://localhost:8000/api_musique/musiques/titre/artiste/album/annee/genre');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function test_Delete_musique_with_id(){
+        $client = static::createClient();
+        $client->request('DELETE', 'https://localhost:8000/api/musiques/delete/2');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function test_Update_musique_with_id_and_parameters(){
+        $client = static::createClient();
+        $client->request('PUT', 'https://localhost:8000/api/musiques/modifier/2/titre/artiste/album/annee/genre');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function test_Get_musique_file_with_id(){
+        $client = static::createClient();
+        $client->request('GET', 'https://localhost:8000/api/musiques/get/musique/2');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function test_Get_image_file_with_id(){
+        $client = static::createClient();
+        $client->request('GET', 'https://localhost:8000/api/musiques/get/image/2');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
     
